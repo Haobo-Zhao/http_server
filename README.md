a1ce35c15ddec8d27c7353128d55c4d16c3c48bc
Haobo-Zhao <haobo4608@gmail.com>
2018-09-28 00:42:27 -0400

    Literally everything needed to run the program.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # http_server
+## Introduction
 A simple http server based on Node.js
+
+Since this is just a simple version, everything, including packages in ./node_modules would be uploaded
+
+## How to run the project
+in the directory, make sure you have installed in your computer and port 8888 is available, then run
+
+node index.js
+
+and visit http://localhost:8888
+
+Boom! (I know, it is not so exciting though...)
--- route.js
+++ route.js
@@ -1,14 +1,13 @@
-function route(handle, pathname, response) {
+function route(handle, pathname, response, request) {
     console.log("About to route a request for " + pathname);
     if (typeof handle[pathname] === 'function') {
-        handle[pathname](response);
+      handle[pathname](response, request);
     } else {
-        // handle pathname no existing currently
-        console.log("No request handler found for " + pathname);
-        response.writeHead(404, { "Content-Type": "text/plain" });
-        response.write("404 Not found");
-        response.end();
+      console.log("No request handler found for " + pathname);
+      response.writeHead(404, {"Content-Type": "text/html"});
+      response.write("404 Not found");
+      response.end();
     }
-}
-
-exports.route = route;
+  }
+  
+  exports.route = route;
